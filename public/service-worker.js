81d7712476c43ebe2ecb185ae1191c7eaa74248d
Muhammad Jabir Khan <muhammadjabirkhan@gmail.com>
2020-08-23 15:42:45 +0500

    First version of 'Everyone's Secrets and Fears'

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,40 +5,59 @@ var URLS_TO_CACHE = [
     "/app.js"
 ];
 
-// Perform install step
-// This step will open a cache and save all the listed paths
-self.addEventListener("install", function (event) {
-    console.log("[Service Worker] install");
-    event.waitUntil(caches.open(CACHE_NAME).then(function (cache) {
-        console.log("[Service Worker] Caching app shell");
-        return cache.addAll(URLS_TO_CACHE);
-    })
-        .then(function () {
-            self.skipWaiting();
-        }));
-});
+// install event
 
-self.addEventListener('activate', function (e) {
-    console.log('[ServiceWorker] Activate');
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            return Promise.all(keyList.map(function (key) {
-                if (key !== CACHE_NAME) {
-                    console.log('[ServiceWorker] Removing old cache', key);
-                    return caches.delete(key);
-                }
-            }));
+  self.addEventListener('install', e => {
+      e.waitUntil(
+          caches.open(CACHE_NAME)
+                  .then(cache => {
+                      return cache.addAll(urlsToCache)
+                                  .then(() =>{
+                                      self.skipWaiting();
+                                  });
+
+                  })
+                  .catch(err => {
+                      console.log('No se ha registrado el cache', err);
+                  })
+      );
+  });
+
+  // activate event
+
+  self.addEventListener('activate', e => {
+      const cacheWhiteList = [CACHE_NAME];
+
+      e.waitUntil(
+          caches.keys()
+                  .then(cacheNames => {
+                      return Promise.all(
+                          cacheNames.map(cacheName => {
+
+                              if(cacheWhiteList.indexOf(cacheName) === -1){
+                                  // Borrar elementos que no se necesitan
+                                  return caches.delete(cacheName);
+
+                              }
+                          })
+
+                      );
+                  })
+                  .then(() => {
+                      //Activar cache
+                      self.clients.claim();
+                  })
+
+      );
+  });
+
+
+  // fetch event
+
+  self.addEventListener('fetch', function(event) {
+      event.respondWith(
+        caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
         })
-    );
-    return self.clients.claim();
-});
-
-// Listen to fetch requests
-self.addEventListener("fetch", function (event) {
-    console.log("[Service Worker] fetch url: " + event.request.url);
-    //console.log(event.request);
-    event.respondWith(caches.match(event.request).then(function (response) {
-        //console.log(response);
-        return response || fetch(event.request);
-    }));
-});
+      );
+    });
