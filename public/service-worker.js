d020e2dcac625e1020f4b042ff8a1819dda4300f
Muhammad Jabir Khan <muhammadjabirkhan@gmail.com>
2020-08-23 15:37:09 +0500

    First version of 'Everyone's Secrets and Fears'

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,44 +1,44 @@
-
-const CACHE_NAME = 'sw-cache-example';
-const toCache = [
-  '/',
-  '/secrets.ejs',
-  '/js/status.js',
+var CACHE_NAME = "yt-appshell-cache-v01";
+var URLS_TO_CACHE = [
+    "/",
+    "/css/styles.css",
+    "/app.js"
 ];
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(toCache)
-      })
-      .then(self.skipWaiting())
-  )
-})
+// Perform install step
+// This step will open a cache and save all the listed paths
+self.addEventListener("install", function (event) {
+    console.log("[Service Worker] install");
+    event.waitUntil(caches.open(CACHE_NAME).then(function (cache) {
+        console.log("[Service Worker] Caching app shell");
+        return cache.addAll(URLS_TO_CACHE);
+    })
+        .then(function () {
+            self.skipWaiting();
+        }));
+});
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request)
-      .catch(() => {
-        return caches.open(CACHE_NAME)
-          .then((cache) => {
-            return cache.match(event.request)
-          })
-      })
-  )
-})
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== CACHE_NAME) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys()
-      .then((keyList) => {
-        return Promise.all(keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removing old cache', key)
-            return caches.delete(key)
-          }
-        }))
-      })
-      .then(() => self.clients.claim())
-  )
-})
+// Listen to fetch requests
+self.addEventListener("fetch", function (event) {
+    console.log("[Service Worker] fetch url: " + event.request.url);
+    //console.log(event.request);
+    event.respondWith(caches.match(event.request).then(function (response) {
+        //console.log(response);
+        return response || fetch(event.request);
+    }));
+});
